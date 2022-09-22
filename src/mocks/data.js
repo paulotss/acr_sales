948edbfed22a4a87847bd6eaee00db8f5ac6aebb
Paulo de Tarso <paulo.oinab@gmail.com>
2022-09-22 13:42:06 -0300

    Cria categorias na home

---
+++ src/mocks/data.js
@@ -0,0 +1,42 @@
+export const categories = [
+  {
+    id: 1,
+    name: "Imóveis",
+    image: "",
+  },
+  {
+    id: 2,
+    name: "Autos e peças",
+    image: "",
+  },
+  {
+    id: 3,
+    name: "Casa",
+    image: "",
+  },
+  {
+    id: 4,
+    name: "Eletrônicos",
+    image: "",
+  },
+  {
+    id: 5,
+    name: "Empregos",
+    image: "",
+  },
+  {
+    id: 6,
+    name: "Serviços",
+    image: "",
+  },
+  {
+    id: 7,
+    name: "Moda e beleza",
+    image: "",
+  },
+  {
+    id: 8,
+    name: "Lazer",
+    image: "",
+  },
+];
--- src/pages/HomePages.js
+++ src/pages/HomePages.js
@@ -1,12 +1,23 @@
 import Head from "../components/Head";
 import SearchBar from "../components/SearchBar";
+import Category from "../components/Category";
+import { categories } from "../mocks/data";
 
 const HomePages = () => {
   return (
     <main>
       <Head />
       <section className="p-10 bg-green-900">
-        <SearchBar />
+        <article>
+          <SearchBar />
+        </article>
+        <article className="flex justify-center mt-10">
+          {
+            categories.map((category) => (
+              <Category key={ category.id } name={ category.name } />
+            ))
+          }
+        </article>
       </section>
     </main>
   )
