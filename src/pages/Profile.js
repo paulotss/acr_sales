2c254f9e65d8359d66f1ec7d56c8986deaca42c1
Paulo de Tarso <paulo.oinab@gmail.com>
2023-03-14 17:04:34 -0300

    Cria lista de produtos em profile

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,13 +1,28 @@
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import Head from "../components/Head";
-//import Adverts from "../components/profile/Adverts";
-import NewAdvertForm from "../components/profile/NewAdvertForm";
 import PersonalData from "../components/profile/PersonalData";
 import Requests from "../components/profile/Requests";
+import Adverts from "../components/profile/Adverts";
+import NewAdvertForm from "../components/profile/NewAdvertForm";
+import { useParams, useLocation } from "react-router-dom";
+import AppContext from "../contexts/AppContext";
 
 const Profile = () => {
-  const [page, setPage] = useState(<NewAdvertForm />)
-  const [active, setActive] = useState(2);
+  const { id } = useParams();
+
+  const pages = [
+    <PersonalData />,
+    <Requests />,
+    <Adverts />,
+    <NewAdvertForm />
+  ];
+
+  const { actProfile, setActProfile } = useContext(AppContext);
+
+  useEffect(() => {
+    setActProfile(Number(id))
+    console.log(actProfile);
+  }, []);
 
   return (
     <main>
@@ -23,34 +38,34 @@ const Profile = () => {
             <li
               className={
                 `p-2 border-2 hover:border-green-900 cursor-pointer
-                ${ active === 0 ? 'bg-green-900 text-white border-green-900' : 'border-green-100'}`
+                ${ actProfile === 0 ? 'bg-green-900 text-white border-green-900' : 'border-green-100'}`
               }
-              onClick={ () =>  { setPage(<PersonalData />); setActive(0) }}
+              onClick={ () =>  { setActProfile(0); console.log(actProfile); }}
             >
               Meus dados
             </li>
             <li
               className={
                 `p-2 border-2 hover:border-green-900 cursor-pointer
-                ${ active === 1 ? 'bg-green-900 text-white border-green-900' : 'border-green-100'}`
+                ${ actProfile === 1 ? 'bg-green-900 text-white border-green-900' : 'border-green-100'}`
               }
-              onClick={ () => { setPage(<Requests />); setActive(1) }}
+              onClick={ () => { setActProfile(1) }}
             >
               Pedidos
             </li>
             <li
               className={
                 `p-2 border-2 hover:border-green-900 cursor-pointer
-                ${ active === 2 ? 'bg-green-900 text-white border-green-900' : 'border-green-100'}`
+                ${ actProfile === 2 || actProfile === 3 ? 'bg-green-900 text-white border-green-900' : 'border-green-100'}`
               }
-              onClick={ () => {setPage(<NewAdvertForm />); setActive(2) }}
+              onClick={ () => { setActProfile(2) }}
             >
               Anúncios
             </li>
           </ul>
         </article>
         <article className="p-10 w-full">
-          { page }
+          { pages[actProfile] }
         </article>
       </section>
     </main>
