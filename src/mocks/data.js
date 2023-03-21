11591a20d6129b1b57c06ed55fb5098f5c6b4fce
Paulo de Tarso <paulo.oinab@gmail.com>
2023-03-21 17:45:16 -0300

    Muda nome imagem categoria

--- src/mocks/data.js
+++ src/mocks/data.js
@@ -83,3 +83,50 @@ export const products = [
     tags: ["manutenção", "serviço"]
   },
 ];
+
+export const mockData = {
+  "reference_id": "ex-00001",
+  "customer": {
+      "name": "Jose da Silva",
+      "email": "email@test.com",
+      "tax_id": "12345678909",
+      "phones": [
+          {
+              "country": "55",
+              "area": "11",
+              "number": "999999999",
+              "type": "MOBILE"
+          }
+      ]
+  },
+  "items": [
+      {
+          "name": "nome do item",
+          "quantity": 1,
+          "unit_amount": 500
+      }
+  ],
+  "qr_codes": [
+      {
+          "amount": {
+              "value": 500
+          },
+          "expiration_date": "2023-08-29T20:15:59-03:00"
+      }
+  ],
+  "shipping": {
+      "address": {
+          "street": "Avenida Brigadeiro Faria Lima",
+          "number": "1384",
+          "complement": "apto 12",
+          "locality": "Pinheiros",
+          "city": "São Paulo",
+          "region_code": "SP",
+          "country": "BRA",
+          "postal_code": "01452002"
+      }
+  },
+  "notification_urls": [
+      "https://meusite.com/notificacoes"
+  ]
+}
