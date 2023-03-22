5e738f1b8db39120c1e1c9808af91fc495fb1e2e
Paulo de Tarso <paulo.oinab@gmail.com>
2023-03-22 20:36:14 -0300

    Adiciona contato de usuário na página de produto

--- src/components/ItemShow.js
+++ src/components/ItemShow.js
@@ -1,9 +1,13 @@
+import axios from '../http'
 import whatsappIcon from '../media/whatsapp.png';
 
 const ItemShow = (props) => {
-
   const { title, price, description, cover } = props;
   const BASE_URL = process.env.REACT_APP_API_BASE_URL;
+
+  const getUser = async () => {
+
+  }
 
   return (
     <article className="flex justify-center">
@@ -22,19 +26,20 @@ const ItemShow = (props) => {
         <p className="mb-3 text-green-900">{ description }</p>
         <div className="p-2 bg-gray-300 border-2 border-green-900">
           <p className="text-xl font-bold text-green-900">Pix: 06.196.928/0001-70</p>
+          <p className="text-xl font-bold text-green-900">Name: {props.userName}</p>
           <div className="font-bold text-green-900 mt-2 flex">
             <a
-              href="https://web.whatsapp.com/send?phone=5561983615753"
+              href={`https://web.whatsapp.com/send?phone=55${props.userWhats}`}
               target="_blank"
               className="w-7 h-7 mr-1"
             >
               <img src={ whatsappIcon } />
             </a>
             <a
-              href="https://web.whatsapp.com/send?phone=5561983615753"
+              href={`https://web.whatsapp.com/send?phone=55${props.userWhats}`}
               target="_blank"
             >
-              (61)98361.5753
+              (61) { props.userWhats }
             </a>
           </div>
         </div>
