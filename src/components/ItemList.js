e26fe4bc858dd0ad5545e418df5e754c5d2bdb92
Paulo de Tarso <paulo.oinab@gmail.com>
2023-05-23 10:15:42 -0300

    ItemList responsive

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -6,22 +6,22 @@ const ItemList = (props) => {
   const BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
   return (
-    <Link to={ `/item/${id}` } className="flex mb-5 flex-row">
+    <Link to={ `/item/${id}` } className="flex mb-5 md:flex-row flex-col">
       <div
-        className="mr-5 w-1/3 h-64 bg-contain bg-center bg-no-repeat bg-gray-300 rounded-xl"
+        className="mr-5 md:w-1/3 w-64 h-64 bg-contain bg-center bg-no-repeat bg-gray-300 rounded-xl"
         style={{
           backgroundImage: `url("https://tebas-bucket.s3.sa-east-1.amazonaws.com/${cover}")`
         }}
       >
       </div>
-      <div className="w-2/3">
+      <div className="md:w-2/3 w-64">
         <div className="flex justify-start text-green-900">
           <h4 className="text-xl">{ title }</h4>
           <h2 className="font-bold text-2xl ml-3">
             { price.toLocaleString('pt-BR', { style:'currency', currency:'BRL' }) }
           </h2>
         </div>
-        <p className="text-green-900 text-sm w-80">{ description }</p>
+        <p className="text-green-900 text-sm w-64">{ description }</p>
       </div>
     </Link>
   )
